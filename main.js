cedf77959446f8e59e9ff1d24ff3c8397b70e9d3
Tilo Stoppe <tilostoppe@gmail.com>
2024-06-10 09:58:31 +0200

    Rendering: show anime titles and images

--- main.js
+++ main.js
@@ -1,12 +1,34 @@
-function fetchData() {
-  var response = fetch("https://api.jikan.moe/v4/anime");
-  return response.json().data;
+async function fetchData() {
+  const response = await fetch("https://api.jikan.moe/v4/anime");
+  return (await response.json()).data;
 }
 
-function render(animes) {
-  // TODO implement
-  console.log(animes)
+async function render(animes) {
+  console.log(animes);
+  animes.forEach((a) => {
+    addElement(a);
+  });
 }
 
-var animes = fetchData();
-render(animes);
+async function run() {
+  const animes = await fetchData();
+  await render(animes);
+}
+
+function addElement(anime) {
+  const animeDiv = document.createElement("div");
+  animeDiv.id = anime.mal_id;
+  const text = document.createTextNode(anime.title);
+  animeDiv.appendChild(text);
+  const animeImg = document.createElement("img");
+  animeImg.src = anime.images.webp.image_url;
+  animeImg.alt = `${anime.title}_img`;
+  animeDiv.setAttribute("class", "anime-wrapper");
+  animeDiv.appendChild(animeImg);
+
+  const appDiv = document.getElementById("app");
+
+  appDiv.appendChild(animeDiv);
+}
+
+run();
