d51ff4ef67b6db08726aed0e6b24183c2c8b4f99
Tilo Stoppe <tilostoppe@gmail.com>
2024-06-10 10:59:57 +0200

    Sort Genres: Sort by average anime score, print via console.log

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
-async function fetchData() {
-  const response = await fetch("https://api.jikan.moe/v4/anime");
+let page = 0;
+const allAnimes = [];
+
+async function fetchData(page = 1) {
+  const response = await fetch(`https://api.jikan.moe/v4/anime?page=${page}`);
   return (await response.json()).data;
 }
 
@@ -11,8 +14,49 @@ async function render(animes) {
 }
 
 async function run() {
-  const animes = await fetchData();
-  await render(animes);
+  for (let index = 0; index < 10; index++) {
+    if (index > 0) {
+      await wait(1000);
+    }
+    page = index + 1;
+    const animes = await fetchData(page);
+    allAnimes.push(...animes);
+    await render(animes);
+  }
+  processAll(allAnimes);
+}
+
+function processAll(animes) {
+  console.log("All Animes", animes);
+
+  const genres = [
+    ...new Set(animes.map((anime) => anime.genres.map((g) => g.name)).flat()),
+  ];
+
+  const genreScores = [];
+
+  for (const genreName of genres) {
+    const animesByGenre = animes.filter((a) =>
+      a.genres.some((g) => g.name === genreName)
+    );
+    const scoreByGenre =
+      Math.round(
+        (animesByGenre.map((a) => a.score).reduce((acc, cur) => acc + cur, 0) /
+          animesByGenre.length) *
+          1000
+      ) / 1000;
+
+    genreScores.push({
+      name: genreName,
+      score: scoreByGenre,
+      animes: animesByGenre,
+    });
+  }
+
+  const sortedGenreScores = genreScores.sort((a, b) =>
+    a.score > b.score ? -1 : 1
+  );
+  console.log("Sorted Genres by Avg Score", sortedGenreScores);
 }
 
 function addElement(anime) {
@@ -22,6 +66,7 @@ function addElement(anime) {
   animeDiv.appendChild(text);
   const animeImg = document.createElement("img");
   animeImg.src = anime.images.webp.image_url;
+  animeImg.setAttribute("loading", "lazy");
   animeImg.alt = `${anime.title}_img`;
   animeDiv.setAttribute("class", "anime-wrapper");
   animeDiv.appendChild(animeImg);
@@ -29,6 +74,14 @@ function addElement(anime) {
   const appDiv = document.getElementById("app");
 
   appDiv.appendChild(animeDiv);
+}
+
+async function wait(ms) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve();
+    }, ms);
+  });
 }
 
 run();
